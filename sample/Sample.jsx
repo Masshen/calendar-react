61a2d7b28edd1c3e81ae97a3f790ca15338435a3
Henock MASS <henock@bpragmatic.fr>
2023-04-07 11:54:20 +0100

    fix

--- sample/Sample.jsx
+++ sample/Sample.jsx
@@ -1,11 +1,21 @@
 import React, { useState } from 'react';
-import Calendar from 'react-calendar';
+import Calendar from '../dist/esm';
 import 'react-calendar/dist/Calendar.css';
 
 import './Sample.css';
 
 export default function Sample() {
   const [value, onChange] = useState(new Date());
+
+  const dates = [];
+  dates.push(new Date(2023, 2, 3));
+  dates.push(new Date(2023, 2, 4));
+  dates.push(new Date(2023, 2, 8));
+  dates.push(new Date(2023, 2, 12));
+  dates.push(new Date(2023, 2, 16));
+  dates.push(new Date(2023, 2, 17));
+  dates.push(new Date(2023, 2, 23));
+  dates.push(new Date(2023, 2, 31));
 
   return (
     <div className="Sample">
@@ -14,7 +24,7 @@ export default function Sample() {
       </header>
       <div className="Sample__container">
         <main className="Sample__container__content">
-          <Calendar onChange={onChange} showWeekNumbers value={value} />
+          <Calendar onChange={onChange} showWeekNumbers value={value} values={dates} />
         </main>
       </div>
     </div>
